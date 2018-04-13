849ed0f7cba38a011e2c95c348a14536de96945f
1150623 <1150623@isep.ipp.pt>
2018-04-13 13:49:41 +0100

    projeto usabilidade start

---
+++ ANADI/ANADI_Usaibilidade_Project/ANADI_Usabilidade/mainScript_Usabilidade.R
@@ -0,0 +1,48 @@
+
+#
+#   Main Script Usabilidade
+#
+
+# GLOBALS
+#Sexo:
+  MASCULINO = "Masculino"
+  FEMININO = "Feminino"
+  SEXO_NAO_DEFINIDO = "Prefiro não dizer"
+
+#Importação ficheiro .csv com os dados do inquerito.
+
+
+library(readr)
+dados <- read_csv("./RESOURCES/Inquérito ANADI.csv")
+View(dados)
+attach(dados)
+
+# Obtencao dados do Género dos inqueridos:
+nr_masculino = 0
+nr_feminino = 0
+nr_SexoNaoDefinido = 0
+for(type in Sexo){
+  if(type == MASCULINO){
+    nr_masculino = nr_masculino + 1
+  }
+  if(type == FEMININO){
+    nr_feminino = nr_feminino + 1
+  }
+  if(type == SEXO_NAO_DEFINIDO){
+    nr_SexoNaoDefinido = nr_SexoNaoDefinido +1
+  }
+}
+# nr_masculino = 38
+# nr_feminino = 31
+# nr_SexoNaoDefinido = 1
+
+# definir valores do pie chart
+graph_sexos_slices = c(nr_masculino, nr_feminino, nr_SexoNaoDefinido)
+graph_sexos_labels = c(MASCULINO, FEMININO, SEXO_NAO_DEFINIDO)
+#extra adicionar percentagens às labels
+percentagens = round(graph_sexos_slices / sum(graph_sexos_slices) * 100)
+graph_sexos_labels = paste(graph_sexos_labels, percentagens)
+graph_sexos_labels = paste(graph_sexos_labels, "%", sep="")
+#desenhar gráfico (pie chart)
+pie(graph_sexos_slices, labels=graph_sexos_labels, col=rainbow(length(graph_sexos_labels)), main = "Género dos Inqueridos")
+  
