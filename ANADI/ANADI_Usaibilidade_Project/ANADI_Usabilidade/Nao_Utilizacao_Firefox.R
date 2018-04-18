5f2d2111b6b57af4489d3f1396c760a241042731
1150623 <1150623@isep.ipp.pt>
2018-04-18 08:58:11 +0100

    No special characters

--- ANADI/ANADI_Usaibilidade_Project/ANADI_Usabilidade/Nao_Utilizacao_Firefox.R
+++ ANADI/ANADI_Usaibilidade_Project/ANADI_Usabilidade/Nao_Utilizacao_Firefox.R
@@ -1,5 +1,4 @@
 
-
 #
 #   Main Script Usabilidade
 #
@@ -14,29 +13,30 @@ UTILIZA_APP = "Sim / Ja utilizei"
 NAO_UTILIZA_APP = "Nao"
 
 
-#Importação ficheiro .csv com os dados do inquerito.
+#Importacao ficheiro .csv com os dados do inquerito.
 
-
-  library(readr)
-  dados <- read_csv("./RESOURCES/Inquérito ANADI.csv")
-  #View(dados)
-  attach(dados)
+library(ggplot2)
+library(plotly)
+library(readr)
+dados <- read_csv("./RESOURCES/Inquerito ANADI.csv")
+#View(dados)
+attach(dados)
 # ----------------------------------------------------
-  
-# Obtencao dados do Género dos inqueridos:
+
+# Obtencao dados do Genero dos inqueridos:
 nr_masculino = 0
 nr_feminino = 0
 nr_SexoNaoDefinido = 0
 
-# Obtencao dados do número de utilizadores da aplicação:
+# Obtencao dados do numero de utilizadores da aplicacao:
 nr_utilizadores = 0
 nr_naoUtilizadores = 0
 nr_Total = nrow(dados)
 col1 = 2; # Genero 
 col5 = 5; # Utilizacao
-#71 é o número total (nr_Total) de inqueridos
+#71 e o numero total (nr_Total) de inqueridos
 M_dados = dados[c(1:nr_Total), c(col1, col5)]
-View(M_dados)
+#View(M_dados)
 
 # M_dados[ INQUIRIDO , COLUNA ] -> Se COLUNA for 1, retorna Genero, se for 2 retorna Utilizacao
 
@@ -45,6 +45,18 @@ nr_fem_nao_utiliza = 0
 nr_masc_utiliza = 0
 nr_masc_nao_utiliza = 0
 
+# Pesquisa numero de utilizadores da aplicacao
+for(user in Utiliza){
+  if(user == UTILIZA_APP){
+      nr_utilizadores = nr_utilizadores + 1
+  }else{
+      nr_naoUtilizadores = nr_naoUtilizadores + 1
+  }
+}
+#nr_utilizadores = 53
+#nr_naoUtilizadores = 18
+
+# Pesquisa genero e utilizacao de cada
 inquirido = 1
 while(inquirido <= nr_Total){
   if(M_dados[inquirido, 1] == FEMININO){
@@ -54,7 +66,8 @@ while(inquirido <= nr_Total){
     }else{
       nr_fem_nao_utiliza = nr_fem_nao_utiliza + 1
     }
-  }else if(M_dados[inquirido, 1] == MASCULINO){
+  }
+  if(M_dados[inquirido, 1] == MASCULINO){
     # Genero = MASCULINO
     if(M_dados[inquirido, 2] == UTILIZA_APP){
       nr_masc_utiliza = nr_masc_utiliza + 1
@@ -62,54 +75,54 @@ while(inquirido <= nr_Total){
       nr_masc_nao_utiliza = nr_masc_nao_utiliza + 1
     }
   }
+  if(M_dados[inquirido, 1] == SEXO_NAO_DEFINIDO){
+    # Utilizadores sem restricao de sexo
+    if(M_dados[inquirido, 2] == UTILIZA_APP){
+      nr_utilizadores = nr_utilizadores + 1
+    }else{
+      nr_naoUtilizadores = nr_naoUtilizadores + 1
+    }
+  }
   inquirido = inquirido + 1
 }
+
 
 nr_feminino = nr_fem_nao_utiliza + nr_fem_utiliza
 nr_masculino = nr_masc_nao_utiliza + nr_masc_utiliza
 nr_SexoNaoDefinido = nr_Total - nr_feminino - nr_masculino
 # -----------------
-#  Gráfico Geral
+#  Grafico Geral
 # -----------------
-    # definir valores do pie chart
-    graph_sexos_slices = c(nr_utilizadores, nr_naoUtilizadores)
-    graph_sexos_labels = c(UTILIZA_APP, NAO_UTILIZA_APP)
-    #extra adicionar percentagens às labels
-    percentagens = round(graph_sexos_slices / sum(graph_sexos_slices) * 100)
-    graph_sexos_labels = paste(graph_sexos_labels," (", percentagens)
-    graph_sexos_labels = paste(graph_sexos_labels, "%)", sep="")
-    #desenhar gráfico (pie chart)
-    pie(graph_sexos_slices, labels=graph_sexos_labels, col=rainbow(length(graph_sexos_labels)), main = "Ja Utilizou Firefox?")
+# definir valores do pie chart
+graph_sexos_slices = c(nr_utilizadores, nr_naoUtilizadores)
+graph_sexos_labels = c(UTILIZA_APP, NAO_UTILIZA_APP)
 
-
+labels_ <- list(
+  labels = graph_sexos_labels, 
+  type = "pie", 
+  values = graph_sexos_slices
+)
+data <- list(labels_)
+layout <- list(title = "Utilizadores Por Genero")
+p <- plot_ly()
+p <- add_trace(p, labels=labels_$labels, type=labels_$type, values=labels_$values)
+p <- layout(p, title=layout$title)
+p
 # ---------------------
-#  Gráfico Por Generos
+#  Grafico Por Generos
 # ---------------------
-    
-    # Para ordenar os resultados obtidos por generos precisamos de saber quantos de cada genero utilizão ou nao firefox.
 
-    library(plotly)
-    
-    mascValues = c(nr_masc_utiliza, nr_masc_nao_utiliza)
-    femValues = c(nr_fem_utiliza, nr_fem_nao_utiliza)
-    
-    
-    Animals <- c("giraffes", "orangutans", "monkeys")
-    SF_Zoo <- c(20, 14, 23)
-    LA_Zoo <- c(12, 18, 29)
-    data <- data.frame(Animals, SF_Zoo, LA_Zoo)
-    
-    p <- plot_ly(data, x = ~Animals, y = ~SF_Zoo, type = 'bar', name = 'SF Zoo') %>%
-      add_trace(y = ~LA_Zoo, name = 'LA Zoo') %>%
-      layout(yaxis = list(title = 'Count'), barmode = 'group')
-    
-    # Create a shareable link to your chart
-    # Set up API credentials: https://plot.ly/r/getting-started
-    chart_link = api_create(p, filename="bar-grouped")
-    chart_link
-    
-    
-    
-    
-    
+# Para ordenar os resultados obtidos por generos precisamos de saber quantos de cada genero utilizao ou nao firefox.
+
+
+Utilizacao <- c("Nao Utiliza", "Utiliza")
+mascValues = c(nr_masc_nao_utiliza, nr_masc_utiliza)
+femValues = c(nr_fem_nao_utiliza, nr_fem_utiliza)
+data <- data.frame(Utilizacao, mascValues, femValues)
+
+p2 <- plot_ly(data, x = ~Utilizacao, y = ~mascValues, type = 'bar', name = 'MASCULINO') %>%
+  add_trace(y = ~femValues, name = 'FEMININO') %>%
+  layout(yaxis = list(title = 'Quantidade'), barmode = 'group')
+p2
+
 
